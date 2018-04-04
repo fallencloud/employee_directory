ea07bde154fd4e7363950de5f61071a30ee88dd2
Sharina V. Jones <sharinavjones@gmail.com>
2018-04-04 03:00:42 -0400

    Employee array bug

--- js/getEmployee.js
+++ js/getEmployee.js
@@ -1,3 +1,7 @@
+//an array to hold employees
+let employees = [];
+
+//a class to store individual employees
 function Employee (icon, name, username, cell, email, location, dob) {
   this.icon = icon;
   this.name = name;
@@ -8,9 +12,7 @@ function Employee (icon, name, username, cell, email, location, dob) {
   this.dob = dob;
 }
 
-let employees = [];
-
-//retreive all results
+//retreive all employees
 $.ajax({
     url: 'https://randomuser.me/api/?results=12',
     dataType: 'json',
@@ -18,7 +20,7 @@ $.ajax({
       //make each into an object
       $.each(data.results, function(i, employee) {
         let newEmp = new Employee(
-          employee.picture.thumbnail,
+          employee.picture,
           employee.name,
           employee.login.username,
           employee.cell,
@@ -30,6 +32,37 @@ $.ajax({
         employees.push(newEmp);
 
       });
-      console.log(employees);
     }
   });
+
+function buildList(employees) {
+  console.log(employees);
+  for (let i = 0; i < employees.length; i++){
+    console.log(employees[i]);
+  }
+
+  //   $.each($employees, function(i, employee) {
+//     console.log(employee);
+//     // let div = document.createElement('div');
+//     // $(div).addClass('employee');
+//     // let img = document.createElement('img');
+//     // img.src = employee.picture.large;
+//     // let name = document.createElement('h3');
+//     // $(name).addClass('name');
+//     // name.textContent = `${employee.name.first} ${employee.name.last}`;
+//     // div.appendChild(img)
+//     //   .appendChild(name);
+//   });//end each
+}
+
+  buildList(employees)
+
+  
+
+//load basic info
+
+
+
+
+
+// };//end build list

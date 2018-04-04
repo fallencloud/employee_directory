3edfcb0885255506e8d398f47c4cd801447c91c5
Sharina V. Jones <sharinavjones@gmail.com>
2018-04-04 02:20:52 -0400

    Retrieve all relevant data

--- js/getEmployee.js
+++ js/getEmployee.js
@@ -1,7 +1,35 @@
+function Employee (icon, name, username, cell, email, location, dob) {
+  this.icon = icon;
+  this.name = name;
+  this.username = username;
+  this.cell = cell;
+  this.email = email;
+  this.location = location;
+  this.dob = dob;
+}
+
+let employees = [];
+
+//retreive all results
 $.ajax({
     url: 'https://randomuser.me/api/?results=12',
     dataType: 'json',
     success: function(data) {
-      console.log(data);
+      //make each into an object
+      $.each(data.results, function(i, employee) {
+        let newEmp = new Employee(
+          employee.picture.thumbnail,
+          employee.name,
+          employee.login.username,
+          employee.cell,
+          employee.email,
+          employee.location,
+          employee.dob
+        );
+        //store the objects in an array
+        employees.push(newEmp);
+
+      });
+      console.log(employees);
     }
   });
